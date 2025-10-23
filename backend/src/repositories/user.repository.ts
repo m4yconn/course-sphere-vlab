8b89378d694b0a3a497c370b798d56608e3053b2
Maycon Santos <mayconzx1@gmail.com>
2025-10-23 10:49:52 -0300

    feat(backend): add user repository

---
+++ backend/src/repositories/user.repository.ts
@@ -0,0 +1,13 @@
+import { UserModel } from "@/model/user.model"
+import { User } from "@/types/user"
+
+export const UserRepository = {
+    
+    create: async (data: User) => {
+        return await UserModel.create(data);
+    },
+
+    findByEmail: async (email: string) => {
+        return await UserModel.findOne({ email });
+    }
+}
